49ab80e9888e23b9b5135ea21850dbbed297fa31
Ramirez ignacio <igramirez@fi.uba.ar>
2024-10-31 11:00:01 -0300

    Acomodo las constantes en el lado del cliente
    Separo una sección con todos los tamaños.

--- client/client.cpp
+++ client/client.cpp
@@ -1,8 +1,8 @@
 #include "client.h"
 
 Client::Client(const char* host, const char* port):
-        protocolo(host, port, is_socket_close), is_socket_close(false), queue_sender(TAMANIO_MAX),
-        queue_receiver(TAMANIO_MAX), game(queue_sender, queue_receiver),
+        protocolo(host, port, is_socket_close), is_socket_close(false), queue_sender(QUEUE_TAMANIO_MAX),
+        queue_receiver(QUEUE_TAMANIO_MAX), game(queue_sender, queue_receiver),
         sender(protocolo, queue_sender), receiver(protocolo, queue_receiver) {
                 sender.start();
                 receiver.start();
--- client/constant_definitons.h
+++ client/constant_definitons.h
@@ -9,6 +9,24 @@
 #define HOST 1
 #define PORT 2
 
+//Tamaños
+const int DUCK_WIDTH = 50;
+const int DUCK_HEIGHT = 50;
+constexpr int HEIGHT_GUN= 20;
+constexpr int WIDTH_GUN= 40;
+constexpr int HEIGHT_GRENADE_BANANA= 13;
+constexpr int WIDTH_GRENADE_BANANA = 15;
+constexpr int HEIGHT_GRANADE_EXPLOTION= 60;
+constexpr int WIDTH_GRANADE_EXPLOTION= 60;
+constexpr int HEIGHT_BULLET= 5;
+constexpr int WIDTH_BULLET= 5;
+const uint8_t WIDTH_ARMOR= 60;
+const uint8_t HEIGHT_ARMOR= 65;
+const uint8_t WIDTH_HELMET= 60;
+const uint8_t HEIGHT_HELMET= 70;
+const int SCENE_WIDTH = 640;
+const int SCENE_HEIGHT = 480;
+
 //protocolo
 const uint8_t FIRST_SEND_BYTE = 0x03;
 const uint8_t FIRST_RECIVE_BYTE = 0x06;
@@ -18,9 +36,6 @@ const uint8_t FULL_GAME_BYTE = 0x20;
 const uint8_t END_ROUND_BYTE = 0x21;
 const uint8_t VICTORY_BYTE = 0x22;
 
-//duck
-const int DUCK_WIDTH = 50;
-const int DUCK_HEIGHT = 50;
 
 //duck movement actions
 const uint8_t MOVEMENT_ACTION = 0x03;
@@ -37,19 +52,13 @@ const uint8_t WEAPON_ACTION = 0x08;
 const uint8_t PICKUP = 0x09;
 const uint8_t LEAVE_GUN= 0x0A;
 const uint8_t SHOOT = 0x0B;
-const int TAMANIO_MAX = 100;
+const int QUEUE_TAMANIO_MAX = 100;
 
 //posicion inicial del pato
 const float POSICION_INICIAL_X = 0.0f;
 const float POSICION_INICIAL_Y = 386;
 
 //armas action
-constexpr int HEIGHT_GUN= 20;
-constexpr int WIDTH_GUN= 40;
-constexpr int HEIGHT_GRENADE_BANANA= 13;
-constexpr int WIDTH_GRENADE_BANANA = 15;
-constexpr int HEIGHT_GRANADE_EXPLOTION= 60;
-constexpr int WIDTH_GRANADE_EXPLOTION= 60;
 constexpr uint8_t C_NOGUN= 0X33;
 constexpr uint8_t C_COWBOY_GUN= 0X34;
 constexpr uint8_t C_COWBOY_BULLET= 0x54;
@@ -73,9 +82,7 @@ constexpr uint8_t C_LASER_RIFLE_GUN= 0X43;
 constexpr uint8_t C_LASER_RIFLE_BULLET= 0x63;
 constexpr uint8_t C_GRANADA_EXPLOTION= 0x64;
 
-//bullet action
-constexpr int HEIGHT_BULLET= 5;
-constexpr int WIDTH_BULLET= 5;
+//bullet orientation
 const uint8_t BULLET_RIGTH = 0x05;
 const uint8_t BULLET_LEFT = 0x06;
 const uint8_t BULLET_UP = 0x50;
@@ -83,17 +90,12 @@ const uint8_t BULLET_UP = 0x50;
 //armadura completa
 const uint8_t C_NOARMOR= 0X73;
 const uint8_t C_ARMOR= 0X74;
-const uint8_t WIDTH_ARMOR= 60;
-const uint8_t HEIGHT_ARMOR= 65;
 const uint8_t C_NOHELMET= 0X75;
 const uint8_t C_HELMET= 0X76;
-const uint8_t WIDTH_HELMET= 60;
-const uint8_t HEIGHT_HELMET= 70;
 
 //escena
 const uint8_t SCENE = 0x0C;
-const int SCENE_WIDTH = 640;
-const int SCENE_HEIGHT = 480;
+
 
 
 
