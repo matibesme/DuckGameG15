5834ed6430d6431c17791c6b6d14d5e8f64a6884
Ramirez ignacio <igramirez@fi.uba.ar>
2024-11-03 14:05:08 -0300

    Agrego la lista de boxes al renderer

--- client/GameRenderer.h
+++ client/GameRenderer.h
@@ -9,6 +9,7 @@
 #include "Items/helmet.h"
 #include "Items/armor.h"
 #include "dto_definitions.h"
+#include "Items/Box.h"
 #include <list>
 
 class GameRenderer {
@@ -20,11 +21,12 @@ private:
     std::list<Armor> armors;
     std::list<Helmet> helmets;
     std::list<Platform> platforms;
+    std::list<Box> boxes;
     static SDL2pp::Rect calcularRectanguloDeZoom(std::list<ClientDuck>& ducks);
     void drawBackground(const uint8_t background_id);
 
 public:
-    explicit GameRenderer(Graficos& graficos, std::list<DTOPlatform>& platforms);
+    explicit GameRenderer(Graficos& graficos, std::list<DTOPlatform>& platforms, std::list<DTOBoxes>& boxes);
 
     void dibujar(Renderer& renderer, GameState& command);
 
