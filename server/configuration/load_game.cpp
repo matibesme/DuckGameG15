1eba051138858ab9ef2c6c721924621c61e0737c
matibesme <matibesme@gmail.com>
2024-11-04 12:13:13 -0300

    subo mapa

--- server/configuration/load_game.cpp
+++ server/configuration/load_game.cpp
@@ -1,23 +1,23 @@
 #include "load_game.h"
-
+#include <iostream>
 
 LoadGameFile::LoadGameFile() {}
 
 void LoadGameFile::loadGame(std::list<DTOPlatform> &platforms) {
     
-    YAML::Node map = YAML::LoadFile("../server/configuration/map.yaml");
-    SCENE_ID=map["scene_id"].as<uint8_t>();
-    POSICION_INICIAL_X = map["duck"][0]["x"].as<float>();
-    POSICION_INICIAL_Y = map["duck"][0]["y"].as<float>();
-    LIFE = map["duck"][0]["life"].as<uint8_t>();
-    RESPAWN_WEAPON_X= map["respawn_weapons_point"][0]["x"].as<float>();
-    RESPAWN_WEAPON_Y= map["respawn_weapons_point"][0]["y"].as<float>();
+    YAML::Node map = YAML::LoadFile("../a_map.yaml");
+    SCENE_ID=map["background_type"].as<uint8_t>();
 
+    POSICION_INICIAL_X=map["duck spawns"][0]["pos_x"].as<float>();
+    POSICION_INICIAL_Y=map["duck spawns"][0]["pos_y"].as<float>();
+    std::cout << POSICION_INICIAL_X << std::endl;
+    std::cout << POSICION_INICIAL_Y << std::endl;
 
-    for (const auto& platform : map["platforms"]) {
-        platforms.emplace_back(DTOPlatform{platform["type"].as<uint8_t>(),
-                                                  platform["x"].as<float>(), platform["y"].as<float>(),
-                                                  platform["width"].as<float>(), platform["height"].as<float>()});
+    for (const auto& platform : map["plataforms"]) {
+        std::string type_str = platform["type"].as<std::string>();
+        uint8_t type_value = static_cast<uint8_t>(type_str[0]);
+        DTOPlatform dto_platform={type_value,platform["pos_x"].as<float>(),platform["pos_y"].as<float>(),platform["width"].as<float>(),platform["height"].as<float>()};
+        platforms.push_back(dto_platform);
     }
 
 }
