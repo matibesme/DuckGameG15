5a6168f83ae245c5e72cf332f77b4583cded6b85
Lou <louramirezalmada@gmail.com>
2024-11-08 11:30:36 -0300

    Se agrega la sección visual de los menus secundarios de crear una partida y unirse a una partida.

--- client/menu/menu.cpp
+++ client/menu/menu.cpp
@@ -1,19 +1,31 @@
 #include "menu.h"
 
 #include <QtWidgets>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 Menu::Menu(QWidget *parent)
     : QMainWindow(parent)
 {
-    initialize_scenes();
+    initialize();
+    show_main_scene();
 }
 
-void Menu::initialize_scenes(){
+void Menu::initialize(){
     main_scene = new QGraphicsScene(this);
+    make_game_scene = new QGraphicsScene(this);
+    join_game_scene = new QGraphicsScene(this);
     view = new QGraphicsView(main_scene, this);
 
+    this->setGeometry(0, 0, 800, 600);
     view->setGeometry(0, 0, 800, 600);
     main_scene->setSceneRect(0, 0, 800, 600);
+    make_game_scene->setSceneRect(0, 0, 800, 600);
+    join_game_scene->setSceneRect(0, 0, 800, 600);
+}
+
+void Menu::show_main_scene(){
+
     QPixmap initial_background("../../data/menu/Background.jpg");
     QPushButton* make_game = new QPushButton("Make game");
     make_game->setGeometry(325, 400, 150, 45);
@@ -34,6 +46,7 @@ void Menu::initialize_scenes(){
                 "   color: black;"
                 "}"
                 );
+    connect(make_game, &QPushButton::pressed, this, &Menu::show_make_game_scene);
     QPushButton* join_game = new QPushButton("Join game");
     join_game->setGeometry(325, 450, 150, 45);
     join_game->setStyleSheet(
@@ -53,6 +66,7 @@ void Menu::initialize_scenes(){
                 "   color: black;"
                 "}"
                 );
+    connect(join_game, &QPushButton::pressed, this, &Menu::show_join_game_scene);
     QPushButton* quit = new QPushButton("Quit");
     quit->setGeometry(350, 500, 100, 45);
     quit->setStyleSheet(
@@ -80,6 +94,122 @@ void Menu::initialize_scenes(){
     view->show();
 }
 
+void Menu::show_make_game_scene(){
+    QPixmap initial_background("../../data/menu/Background.jpg");
+
+    //Columna 1
+    QWidget *widget_players = new QWidget;
+    QVBoxLayout *layout_players = new QVBoxLayout(widget_players);
+
+    QWidget *widget_choose_players = new QWidget;
+    QHBoxLayout *layout_choose_players = new QHBoxLayout(widget_choose_players);
+
+    QWidget *widget_name_players = new QWidget;
+    QVBoxLayout *layout_name_players = new QVBoxLayout(widget_name_players);
+
+    QPushButton* single_player = new QPushButton("Single player");
+    QPushButton* two_players = new QPushButton("Two players");
+    layout_choose_players->addWidget(single_player);
+    layout_choose_players->addWidget(two_players);
+
+    layout_name_players->addWidget(new QLabel("Enter your name, player 1"));
+    QLineEdit* name_player_1 = new QLineEdit();
+    layout_name_players->addWidget(name_player_1);
+
+    layout_name_players->addWidget(new QLabel("Enter your name, player 2"));
+    QLineEdit* name_player_2 = new QLineEdit();
+    layout_name_players->addWidget(name_player_2);
+
+    layout_players->addWidget(widget_choose_players);
+    layout_players->addWidget(widget_name_players);
+
+    widget_players->setGeometry(150, 200, 200, 100);
+
+    //Columna 2
+    QWidget *widget_game = new QWidget;
+    QVBoxLayout *layout_game = new QVBoxLayout(widget_game);
+
+    layout_game->addWidget(new QLabel("IP Adress"));
+    QLineEdit* adress = new QLineEdit();
+    layout_game->addWidget(adress);
+    layout_game->addWidget(new QLabel("Port"));
+    QLineEdit* port = new QLineEdit();
+    layout_game->addWidget(port);
+    QPushButton* make_game_button = new QPushButton("Make game");
+    layout_game->addWidget(make_game_button);
+    QPushButton* back_button = new QPushButton("Back");
+    layout_game->addWidget(back_button);
+    connect(back_button, &QPushButton::pressed, this, &Menu::show_main_scene);
+    widget_game->setGeometry(400, 200, 200, 100);
+
+    make_game_scene->addWidget(widget_players);
+    make_game_scene->addWidget(widget_game);
+    make_game_scene->setBackgroundBrush(initial_background);
+    view->setScene(make_game_scene);
+    view->show();
+}
+
+void Menu::show_join_game_scene(){
+    QPixmap initial_background("../../data/menu/Background.jpg");
+
+    //Columna 1
+    QWidget *widget_players = new QWidget;
+    QVBoxLayout *layout_players = new QVBoxLayout(widget_players);
+
+    QWidget *widget_choose_players = new QWidget;
+    QHBoxLayout *layout_choose_players = new QHBoxLayout(widget_choose_players);
+
+    QWidget *widget_name_players = new QWidget;
+    QVBoxLayout *layout_name_players = new QVBoxLayout(widget_name_players);
+
+    QPushButton* single_player = new QPushButton("Single player");
+    QPushButton* two_players = new QPushButton("Two players");
+    layout_choose_players->addWidget(single_player);
+    layout_choose_players->addWidget(two_players);
+
+    layout_name_players->addWidget(new QLabel("Enter your name, player 1"));
+    QLineEdit* name_player_1 = new QLineEdit();
+    layout_name_players->addWidget(name_player_1);
+
+    layout_name_players->addWidget(new QLabel("Enter your name, player 2"));
+    QLineEdit* name_player_2 = new QLineEdit();
+    layout_name_players->addWidget(name_player_2);
+
+    layout_players->addWidget(widget_choose_players);
+    layout_players->addWidget(widget_name_players);
+
+    widget_players->setGeometry(150, 200, 200, 100);
+
+    //Columna 2
+    QWidget *widget_games = new QWidget;
+    QVBoxLayout *layout_games = new QVBoxLayout(widget_games);
+
+
+    QComboBox *game_options = new QComboBox();
+    game_options->addItem("Partida1");
+    game_options->addItem("Partida2");
+    game_options->addItem("Partida3");
+    layout_games->addWidget(new QLabel("Select a game:"));
+    layout_games->addWidget(game_options);
+    QPushButton* join_game_button = new QPushButton("Join game");
+    layout_games->addWidget(join_game_button);
+    QPushButton* back_button = new QPushButton("Back");
+    connect(back_button, &QPushButton::pressed, this, &Menu::show_main_scene);
+    layout_games->addWidget(back_button);
+
+    widget_games->setGeometry(400, 200, 200, 100);
+
+    join_game_scene->addWidget(widget_games);
+    join_game_scene->addWidget(widget_players);
+    join_game_scene->setBackgroundBrush(initial_background);
+    view->setScene(join_game_scene);
+    view->show();
+
+}
+
+void show_number_players(){
+
+}
 Menu::~Menu()
 {
 }
--- client/menu/menu.h
+++ client/menu/menu.h
@@ -10,8 +10,14 @@ class Menu : public QMainWindow
 private:
     QGraphicsView* view;
     QGraphicsScene* main_scene;
+    QGraphicsScene* make_game_scene;
+    QGraphicsScene* join_game_scene;
 
-    void initialize_scenes();
+    void initialize();
+    void show_main_scene();
+    void show_make_game_scene();
+    void show_join_game_scene();
+    void show_number_players();
 
 public:
     Menu(QWidget *parent = nullptr);
