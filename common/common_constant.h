a2fe16e209c7abb18315044bc74e4da5592bf439
earagon04 <earagon@fi.uba.ar>
2024-11-17 15:42:24 -0300

    arreglando change weapon, falla de surface

--- common/common_constant.h
+++ common/common_constant.h
@@ -137,7 +137,6 @@ constexpr uint8_t LISTAR_PARTIDAS = 0xFF;
 constexpr uint8_t MATCH_WITH_SAME_NAME = 0xBB;
 
 //cheat codes
-constexpr uint8_t CHEAT_CHANGE_WEAPON = 0x110;
-constexpr uint8_t CHEAT_STOP_CHANGE_WEAPON = 0x111;
-constexpr uint8_t CHEAT_SPAWN_BOX = 0x112;
-constexpr uint8_t CHEAT_SPAWN_ARMOR = 0x113;
+constexpr uint8_t CHEAT_CHANGE_WEAPON = 0x0B;
+constexpr uint8_t CHEAT_SPAWN_BOX = 0x0C;
+constexpr uint8_t CHEAT_SPAWN_ARMOR = 0x0D;
