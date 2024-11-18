49af8985e8d3802123095b55bed12cf04a8711ee
Lou <louramirezalmada@gmail.com>
2024-11-18 13:22:13 -0300

    Cambios en los comandos del instalador al copiar archivos

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ cmake --build .
 ./taller_tests
 
 #Mover archivos
-sudo mkdir /var/TallerDuckGame && sudo cp -p taller_client taller_server taller_editor /var/TallerDuckGame
+sudo mkdir /var/TallerDuckGame && sudo cp -p -f taller_client taller_server taller_editor /var/TallerDuckGame
 cd ..
-sudo mkdir /etc/TallerDuckGame && sudo cp -p  /data/editor/menu_config.yaml /server/configuration/config.yaml /etc/TallerDuckGame
-sudo cp -p data /usr/bin
+sudo mkdir /etc/TallerDuckGame && sudo cp -p -f  /data/editor/menu_config.yaml /server/configuration/config.yaml /etc/TallerDuckGame
+sudo cp -p -f data /usr/bin
