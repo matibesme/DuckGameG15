981683cb8a55f0b53723f95261ca9240650352cd
Lou <louramirezalmada@gmail.com>
2024-11-19 18:49:42 -0300

    Se agrega el comando de los tests

--- download.sh
+++ download.sh
@@ -49,3 +49,5 @@ sudo apt install qtcreator qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
 sudo apt install libqt5charts5-dev libqt5datavisualization5-dev libqt5gamepad5-dev libqt5gstreamer-dev libqt5networkauth5-dev libqt5opengl5-dev libqt5remoteobjects5-dev libqt5scxml5-dev libqt5sensors5-dev libqt5serialbus5-dev libqt5serialport5-dev libqt5svg5-dev libqt5texttospeech5-dev libqt5virtualkeyboard5-dev libqt5waylandclient5-dev libqt5waylandcompositor5-dev libqt5webkit5-dev libqt5webchannel5-dev libqt5websockets5-dev libqt5webview5-dev libqt5x11extras5-dev libqt5xmlpatterns5-dev
 #YAML
 sudo apt-get install libyaml-cpp-dev
+#TESTS
+sudo apt-get install libgtest-dev
