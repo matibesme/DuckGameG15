4ea00c2702a47d64ab3150d137094a260cb206cb
Lou <louramirezalmada@gmail.com>
2024-11-20 13:29:00 -0300

    Ahora los items aparecen en el lugar donde fueron llamados por el menú.

--- editor/levelEditor/leveleditor.cpp
+++ editor/levelEditor/leveleditor.cpp
@@ -84,18 +84,18 @@ void LevelEditor::initialize_menus(const QPoint &pos) {
       QAction *action = new QAction(
           QString::fromStdString(option.as<std::string>()), submenu_option);
       connect(action, &QAction::triggered,
-              [this, action, submenu_option, submenu_name]() {
+              [this, action, submenu_option, submenu_name, pos]() {
                 QString action_name = action->text();
                 if (submenu_name == "Weapons") {
-                  controller->set_spawn_weapon(action_name);
+                  controller->set_spawn_weapon(action_name, pos.x(), pos.y());
                 } else if (submenu_name == "Platforms") {
-                  controller->set_platform(action_name);
+                  controller->set_platform(action_name, pos.x(), pos.y());
                 } else if (submenu_name == "Walls") {
-                  controller->set_wall(action_name);
+                  controller->set_wall(action_name, pos.x(), pos.y());
                 } else if (submenu_name == "Backgrounds") {
                   controller->set_background(action_name);
                 } else if (submenu_name == "Armour") {
-                  controller->set_spawn_armour(action_name);
+                  controller->set_spawn_armour(action_name, pos.x(), pos.y());
                 }
               });
       submenu_option->addAction(action);
@@ -112,7 +112,9 @@ void LevelEditor::initialize_menus(const QPoint &pos) {
 
   QAction *action_itemBox_spawn = new QAction("itemBox", this);
   connect(action_itemBox_spawn, &QAction::triggered,
-          [this, action_itemBox_spawn]() { controller->set_spawn_box(); });
+          [this, action_itemBox_spawn, pos]() {
+            controller->set_spawn_box(pos.x(), pos.y());
+          });
   menu.addAction(action_itemBox_spawn);
 
   QAction *action_save = new QAction("Save", this);
